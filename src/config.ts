ad2a75997f2ef8f5952b96b16cc879d454517614
David Yao <davidyao@Davids-MacBook-Air.local>
2024-01-04 14:36:35 -0600

    edited config file

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,1 @@
-import crypto from 'crypto';
-// Generate a secure secret key of a certain length (e.g., 32 bytes)
-// export const secretKey: string = crypto.randomBytes(32).toString('hex');
-// give me a constant key so tht it's the same even after restarting the app
 export const secretKey: string = 'a4c1c2d5b3a6c4d5b3a6c4d5b3a6c4d5';
